b0030a0cd4d56f941632bafc80403ae740af6c03
ingseo <74130681+ingseo@users.noreply.github.com>
2022-11-10 22:39:21 +0900

    비밀 정보 보호

---
+++ config/prod.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI : process.env.MONGO_URI // 이름을 MONGO_URI로 해서 관리해줘야함. 이름을 바꿀거면 같이 바꿔야함!
+}
--- index.js
+++ index.js
@@ -1,17 +1,48 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://inseo:ehxhfl@bolierplate.ttnkqah.mongodb.net/?retryWrites=true&w=majority', {
-    // useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false 
-    // 써주는 이유 : 오류안나게 하려고!..인데 쓰니까 오류나네..
+const config = require('./config/key')
+
+const { User } = require('./models/User');
+
+//bodyparser = 클라이언트에서 오는 정보를 분석해서 가져올 수 있게 해주는 역할
+app.use(bodyParser.urlencoded({extended: true})); // application/x-www-form-urlencoded 타입의 데이터를 분석
+app.use(bodyParser.json()); // application/jseon 타입의 데이터를 분석
+
+mongoose.connect(config.mongoURI, {
+    // 몽고DB 연결
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+    // 써주는 이유 : 오류안나게 하려고!
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log('err'))
 
 app.get('/', (req, res) => {
   res.send('Hello World! 안녕하세요~')
 })
+
+app.post('/register', (req, res)  => {
+  // 목표 : 회원 가입 할 때 필요한 정보들을 client에서 가져오면 그것들을 데이터 베이스에 넣어준다.
+  
+    const user = new User(req.body) //request body로 클라이언트에 보내는 정보를 받는다.
+
+    user.save((err, userInfo) => {//몽고DB에서 오는 메소드. save. 정보들이 user 모델에 저장.
+      if(err) return res.jseon({success: false, err}) //에러가 났을 때 에러메시지 전달
+      return res.status(200).json({ // status(200) = 성공
+        success: true
+      })
+    })
+})
+
+
+
+
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
