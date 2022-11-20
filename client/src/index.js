edea2d7634b7a48332e984d679fdda47f7e35f29
ingseo <74130681+ingseo@users.noreply.github.com>
2022-11-21 03:24:23 +0900

    redux 설치

--- client/src/index.js
+++ client/src/index.js
@@ -3,15 +3,29 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+// import { DatePicker } from 'antd';
+import { legacy_createStore as createStore, applyMiddleware } from 'redux'
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import Reducer from './_reducer';
 
-import { DatePicker } from 'antd';
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
+    <Provider 
+      store={createStoreWithMiddleware(Reducer,
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+      )}
+    >
+        <App />
+    </Provider>,
+  );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
