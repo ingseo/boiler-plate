d21ee2e7d114d4aaaedecaff1d17775b02f3595e
ingseo <74130681+ingseo@users.noreply.github.com>
2022-11-29 09:55:54 +0900

    로그아웃버튼 문제 해결

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -7,19 +7,12 @@ function LandingPage() {
 
   const navigate = useNavigate();
   const user = useSelector(state => state.user)
+  const isAuthBtn = user.userData.isAuth 
 
   //랜딩페이지에 들어오자마자, 아래 함수를 실행한다.
     useEffect(() => { // useEffect 함수는 리액트 컴포넌트가 렌더링 될 때마다 특정 작업을 실행할 수 있도록 하는 Hook이다.
       axios.get('/api/hello') //get request를 서버에다 보낸다. 그것의 엔드포인트는 해당 주소이다.
       .then(response => console.log(response.data)) //보낸 다음에 서버에서 돌아오는 response를 콘솔창에 보여준다.
-      
-      console.log(user.userData.isAuth)
-      
-      if(user.userData.isAuth == false){
-        document.getElementById('logoutBtn').style.display = 'none';
-      } else if(user.userData.isAuth == true) {
-        document.getElementById('logoutBtn').style.display = 'block';
-      }
     }, [])
 
     const onClickHandler = () => {
@@ -42,10 +35,18 @@ function LandingPage() {
     
     <h2>시작 페이지</h2>
     <div style={{marginTop: '10px'}}>
-      <button id='logoutBtn' onClick={onClickHandler} style={{
-        cursor: 'pointer', border: '2px solid #333', borderRadius: '10px',
-        padding: '5px', fontWeight: 'bold', backgroundColor: '#999', color: '#fff',
-      }}>Logout</button>
+      {isAuthBtn ? (
+          <>
+            <button id='logoutBtn' onClick={onClickHandler} style={{
+              cursor: 'pointer', border: '2px solid #333', borderRadius: '10px',
+              padding: '5px', fontWeight: 'bold', backgroundColor: '#999', color: '#fff',
+            }}>Logout</button>
+          </>
+        ) : (
+          <>
+          </>
+        )}
+      
     </div>
     </div>
   )
